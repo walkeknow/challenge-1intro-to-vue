4fde23094798e7a888658a8d0ac6e3f1b2a9618a
Kewal Shah <kewalshah077@gmail.com>
2022-05-31 22:09:01 -0400

    feat: L2

--- dist/main.js
+++ dist/main.js
@@ -7,6 +7,9 @@ var app = new Vue({
   // NOTE Vue is reactive
   data: {
     product: 'Socks',
-    description: 'Christmas Socks!'
+    description: 'Christmas Socks!',
+    image: './assets/vmSocks-green-onWhite.jpeg',
+    altText: 'Green socks',
+    memeUrl: 'https://res.cloudinary.com/practicaldev/image/fetch/s--TlyEacHW--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_auto%2Cw_880/https://dev-to-uploads.s3.amazonaws.com/i/vqe1zzbixk1bc6amhtk2.png'
   } 
 })
