c4741091db4aae07fbf585f6d717dcb36ff5c167
Kewal Shah <kewalshah077@gmail.com>
2022-06-08 00:29:03 -0400

    feat: L6

--- dist/main.js
+++ dist/main.js
@@ -10,6 +10,8 @@ var app = new Vue({
     description: "Christmas Socks!",
     image: "./assets/vmSocks-green-onWhite.jpeg",
     altText: "Green Socks",
+    inStock: true,
+    disabledButton: 'disabledButton',
     memeUrl:
       "https://res.cloudinary.com/practicaldev/image/fetch/s--TlyEacHW--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_auto%2Cw_880/https://dev-to-uploads.s3.amazonaws.com/i/vqe1zzbixk1bc6amhtk2.png",
     inventory: 11,
